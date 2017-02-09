e7b972a51008a759b5fc6ba2f7a1a02679bbb7c2
ifsp.itp <ifsp.itp@gmail.com>
2017-02-09 13:58:12 -0200

    HelloWorld

---
+++ app/src/main/java/br/edu/ufscar/tmandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package br.edu.ufscar.tmandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by danilocbueno on 09/02/17.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
