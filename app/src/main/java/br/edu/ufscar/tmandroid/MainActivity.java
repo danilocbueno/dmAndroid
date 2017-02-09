3d8255a95129eb1dacb61ac7fcf92a0a41f50f3b
ifsp.itp <ifsp.itp@gmail.com>
2017-02-09 14:12:27 -0200

    HelloWorld2.0

--- app/src/main/java/br/edu/ufscar/tmandroid/MainActivity.java
+++ app/src/main/java/br/edu/ufscar/tmandroid/MainActivity.java
@@ -2,6 +2,10 @@ package br.edu.ufscar.tmandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by danilocbueno on 09/02/17.
@@ -9,9 +13,23 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //variaveis para os elementos da view
+    private EditText nomeEditText;
+    private TextView saudacaoTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //referenciando os elementos
+        this.nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+        this.saudacaoTextView = (TextView) findViewById(R.id.saudacaoTextView);
+    }
+
+    public void surpreenderUsuario(View v) {
+        Editable texto = this.nomeEditText.getText();
+        this.saudacaoTextView.setText(texto);
+
     }
 }
