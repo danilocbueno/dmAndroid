f25f873b7a828009da47c64eebad8d4a54a6cbcc
ifsp.itp <ifsp.itp@gmail.com>
2017-02-21 09:05:48 -0300

    HelloWorld Melhorado

--- app/src/main/java/br/edu/ufscar/tmandroid/MainActivity.java
+++ app/src/main/java/br/edu/ufscar/tmandroid/MainActivity.java
@@ -1,7 +1,16 @@
 package br.edu.ufscar.tmandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.File;
 
 /**
  * Created by danilocbueno on 09/02/17.
@@ -9,9 +18,36 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
+    //variaveis para os elementos da view
+    private EditText nomeEditText;
+    private TextView saudacaoTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //referenciando os elementos
+        this.nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+        this.saudacaoTextView = (TextView) findViewById(R.id.saudacaoTextView);
+    }
+
+    public void surpreenderUsuario(View v) {
+        Editable texto = this.nomeEditText.getText();
+        this.saudacaoTextView.setText(texto);
+
+    }
+
+    public void abrirNavegador(View v) {
+        Uri	uri	=	Uri.parse("http://www.android.com");
+        Intent	intent	=	new	Intent(Intent.ACTION_VIEW,	uri);
+        startActivity(intent);
+    }
+
+    public void tirarFoto(View v) {
+        Uri uri = Uri.fromFile(new File("/sdcard/hello.jpg"));
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivity(intent);
     }
 }
